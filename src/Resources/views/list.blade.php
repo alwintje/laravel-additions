51cbef4213041df5382b80f3031ce14c0afd3b69
alwintje <alwinkroesen@gmail.com>
2024-11-05 09:30:48 +0100

    feat: per page

--- src/Resources/views/list.blade.php
+++ src/Resources/views/list.blade.php
@@ -1,9 +1,11 @@
-<div class="text-center" style="position: relative;min-height: 60px;">
+<div class="text-center" @if(isset($perPage)) style="position: relative;min-height: 60px;" @endif >
     {{ $results->links() }}
+    @if(isset($perPage))
     <div style="position: absolute;right:0;top: 0;">
         <label style="margin: 0;" for="per-page-select">{!! __('global.Rows_per_page') !!}</label><br />
-        <x-laravel-additions::per-page :sizes="[0, 1, 15, 30, 50, 100, 250, 500]" :current="$perPage" style="width: auto;float: right;" id="per-page-select" />
+        <x-laravel-additions::per-page :sizes="$perPageOptions ?? [15, 30, 50, 100, 250, 500]" :current="$perPage" style="width: auto;float: right;" id="per-page-select" />
     </div>
+    @endif
 </div>
 @yield('content')
 <div class="text-center">
