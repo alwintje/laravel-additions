2ed5a338097d734306b9e59ed7c8aef7839b70d4
alwintje <alwinkroesen@gmail.com>
2025-07-22 12:06:43 +0200

    Add filter helper functions

--- src/Models/Contenter/ContenterField.php
+++ src/Models/Contenter/ContenterField.php
@@ -4,6 +4,9 @@ namespace Kroesen\LaravelAdditions\Models\Contenter;
 
 use Closure;
 use Exception;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\Query\Builder;
 
 class ContenterField
 {
@@ -12,6 +15,7 @@ class ContenterField
     public const APPLICABLE_NEGATIVE_NUMBER = 'negative number';
     public const APPLICABLE_NOT_EMPTY = 'not empty';
     public bool|Closure $applicable;
+
     public function __construct(
         protected string    $name,
         protected mixed     $default,
@@ -20,43 +24,45 @@ class ContenterField
 
     )
     {
-        if($this->name === 'sorting'){
+        if ($this->name === 'sorting') {
             throw new Exception('Field "sorting" is reserved and cannot be used');
         }
-        if($applicable === self::APPLICABLE_TEXT){
+        if ($applicable === self::APPLICABLE_TEXT) {
             $this->applicable = fn($builder, $data) => $data !== null && $data !== '';
-        }elseif($applicable === self::APPLICABLE_POSITIVE_NUMBER){
-            $this->applicable = fn($builder, $data) => $data !== null && ((int) $data) > 0;
-            if($this->action === null){
-                $this->action = fn($builder, &$data) => $data = (int) $data;
+        } elseif ($applicable === self::APPLICABLE_POSITIVE_NUMBER) {
+            $this->applicable = fn($builder, $data) => $data !== null && ((int)$data) > 0;
+            if ($this->action === null) {
+                $this->action = fn($builder, &$data) => $data = (int)$data;
             }
-        }elseif($applicable === self::APPLICABLE_NEGATIVE_NUMBER){
-            $this->applicable = fn($builder, $data) => $data !== null && ((int) $data) < 0;
-            if($this->action === null){
-                $this->action = fn($builder, &$data) => $data = (int) $data;
+        } elseif ($applicable === self::APPLICABLE_NEGATIVE_NUMBER) {
+            $this->applicable = fn($builder, $data) => $data !== null && ((int)$data) < 0;
+            if ($this->action === null) {
+                $this->action = fn($builder, &$data) => $data = (int)$data;
             }
-        }elseif($applicable === self::APPLICABLE_NOT_EMPTY){
+        } elseif ($applicable === self::APPLICABLE_NOT_EMPTY) {
             $this->applicable = fn($builder, $data) => !empty($data);
-        }else{
+        } else {
             $this->applicable = $applicable;
         }
     }
 
     public static function make(
-        string $name,
-        mixed $default,
+        string              $name,
+        mixed               $default,
         Closure|bool|string $applicable,
-        Closure $action
-    ): static {
+        Closure             $action
+    ): static
+    {
         return new static($name, $default, $applicable, $action);
     }
+
     public static function search(string $name, Closure $action): static
     {
         return new static(
             $name,
             '',
             self::APPLICABLE_TEXT,
-            fn ($query, $search) => $action($query, $search === null ? null : trim($search)),
+            fn($query, $search) => $action($query, $search === null ? null : trim($search)),
         );
     }
 
@@ -64,6 +70,7 @@ class ContenterField
     {
         return new static('perPage', $default, true, null);
     }
+
     public static function page(int $default = 1): static
     {
         return new static('page', $default, true, null);
@@ -73,30 +80,29 @@ class ContenterField
     {
 
         $default = [];
-        foreach ($options as $option => $enabled){
-            if(is_bool($enabled)){
+        foreach ($options as $option => $enabled) {
+            if (is_bool($enabled)) {
                 $default[$option] = $enabled;
-            }else{
+            } else {
                 $default[$enabled] = true;
             }
         }
 
-        if($action === null){
-            $action = function($builder, $mustExists, $mustNotExists) use ($name) {
+        if ($action === null) {
+            $action = function ($builder, $mustExists, $mustNotExists) use ($name) {
                 $builder
                     ->whereIn($name, $mustExists)
-                    ->whereNotIn($name, $mustNotExists)
-                ;
+                    ->whereNotIn($name, $mustNotExists);
             };
         }
 
-        $action = function($builder, $data) use ($action) {
+        $action = function ($builder, $data) use ($action) {
             $mustNotExists = [];
             $mustExists = [];
-            foreach ($data as $key => $bool){
-                if($bool === 'true'){
+            foreach ($data as $key => $bool) {
+                if ($bool === 'true') {
                     $mustExists[] = $key;
-                }else{
+                } else {
                     $mustNotExists[] = $key;
                 }
             }
@@ -130,7 +136,78 @@ class ContenterField
             ) || (
                 is_callable($this->applicable)
                 && call_user_func($this->applicable, $builder, $data)
-            )
-        ;
+            );
+    }
+
+    public static function addToWith(EloquentBuilder|Builder|Relation $builder, string|array $relations, \Closure $callback): void
+    {
+        if (is_string($relations)) {
+            $relations = [$relations];
+        }
+        $eagerLoads = $builder->getEagerLoads();
+        foreach ($relations as $relation) {
+            $builderCopy = $builder;
+            if (!isset($eagerLoads[$relation])) {
+                if (!str_contains($relation, '.')) {
+                    return;
+                }
+                $parts = explode('.', $relation);
+                $part = array_shift($parts);
+
+                if (isset($eagerLoads[$part])) {
+                    $previousClosure = $eagerLoads[$part];
+                    $builderCopy->with([
+                        $part => function (Relation $query) use ($parts, $callback, $previousClosure) {
+                            $previousClosure($query);
+                            self::addToWith($query, implode('.', $parts), $callback);
+                        }
+                    ]);
+                }
+
+                continue;
+            }
+
+            $previousClosure = $eagerLoads[$relation];
+            $builder->with([
+                $relation => function ($query) use ($callback, $previousClosure) {
+                    $previousClosure($query);
+                    $callback($query);
+                }
+            ]);
+        }
+    }
+
+    public static function addToWhereHas(EloquentBuilder|Builder|Relation $builder, string|array $relations, \Closure $callback): void
+    {
+        if (is_string($relations)) {
+            $relations = [$relations];
+        }
+        if (method_exists($builder, 'getQuery')) {
+            $builder = $builder->getQuery();
+        }
+        $wheres = $builder->wheres;
+        foreach ($wheres as $where) {
+
+            if ($where['type'] === 'Exists' && isset($where['query']) && in_array($where['query']->from, $relations)) {
+                /** @var Builder $query */
+                $query = $where['query'];
+                $callback($query);
+            } elseif ($where['type'] === 'Nested') {
+                self::addToWhereHas($where['query'], $relations, $callback);
+            }
+        }
+    }
+
+    public static function addToRawOrder(EloquentBuilder|Builder|Relation $builder, string $detect, string $after, string $toAdd): void
+    {
+        if(method_exists($builder, 'getQuery')){
+            $builder = $builder->getQuery();
+        }
+        foreach ($builder->orders as $k => $order) {
+            if(isset($order['type']) && $order['type'] === 'Raw' && str_contains($order['sql'], $detect)){
+                $order['sql'] = str_replace($after, "$after and ($toAdd)", $order['sql']);
+                $builder->orders[$k] = $order;
+            }
+        }
     }
 }
